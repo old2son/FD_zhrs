d6dffda25ff0fcea1dc3aed4a05c83980d943e9d
CJY <13632362832@163.com>
2025-11-14 23:37:52 +0800

    Update state.ts
    Made properties optional in IState interface.

--- src/stores/interface/state.ts
+++ src/stores/interface/state.ts
@@ -2,8 +2,8 @@ export interface IState {
     referrer: string;
     keyword: string;
     sign: string;
-    token: string;
-    appID: string;
+    token?: string;
+    appID?: string;
     txVideoSign?: string;
-    licenseUrl: string;
+    licenseUrl?: string;
 }
